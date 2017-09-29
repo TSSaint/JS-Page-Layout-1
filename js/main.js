b65ba546a4be61fccaa379560cba920ad1e4a709
Thomas Santos <thomas@socialeffort.co>
2017-09-29 17:10:18 -0400

    add js fades and button click functionality

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
 // init jquery
-function main(){}
+function main(){
+  // get skillset class
+  // var $skillset = $('.skillset');
+
+  // hide class skillset
+  $('.skillset').hide();
+  // add fade in
+  $('.skillset').fadeIn(1000);
+  // hide projects
+  $('.projects').hide();
+  // add click functionality to projects button instances
+  $('.projects-button').on('click',function() {
+
+  });
+}
 $(document).ready(main);
